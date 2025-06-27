9e5f3d7e10ae562d81446b85d0f5884d32cb3382
YiGunHa <isinegajok@naver.com>
2025-06-27 08:09:02 +0800

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // .env에 선언됨
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-});
-
-module.exports = pool;
--- backend/hash.js
+++
@@ -1,8 +0,0 @@
-// hash.js
-const bcrypt = require('bcrypt');
-
-const plainPassword = '1234'; // 원하는 비밀번호
-bcrypt.hash(plainPassword, 10, (err, hash) => {
-  if (err) throw err;
-  console.log('✅ 생성된 해시:', hash);
-});
