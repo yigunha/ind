f30a4c93b2e6c05eae23e571450d8774b9c5a425
YiGunHa <isinegajok@naver.com>
2025-07-13 06:16:05 +0900

    error-0011

--- pages/login.js
+++ pages/login.js
@@ -79,7 +79,7 @@ export default function LoginPage() {
           display: flex;
           flex-direction: column;
           align-items: center;
-          padding-top: 10vh;
+          min-height: 10vh;
           background-color: #f0f2f5;
           font-family: Arial, sans-serif;
         }
